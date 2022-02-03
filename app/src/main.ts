75f786011000bcc7c98064d0e3dea537639be953
ML6503 <ml6503@gmail.com>
2022-02-04 01:58:46 +0200

    feat: add swager config

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFastifyApplication } from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino';
 import { config } from './common/config';
@@ -17,6 +18,15 @@ async function bootstrap() {
     bufferLogs: true,
   });
   app.useLogger(app.get(Logger));
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(config.PORT, '0.0.0.0', () =>
     console.log(`Server listen on port ${config.PORT}`),
   );
